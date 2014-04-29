dd08d9df360f8d005e652e5fff7c15461d568ef1
Kazuki AKAMINE <kazzone87@gmail.com>
2014-04-29 19:47:24 +0900

    Use Class msgpack

--- src/main/scala/net/animetick/scala_msgpack_tutor/Server.scala
+++ src/main/scala/net/animetick/scala_msgpack_tutor/Server.scala
@@ -3,28 +3,42 @@ package net.animetick.scala_msgpack_tutor
 import org.msgpack.scalarpc.ScalaServer
 
 import org.msgpack.scalarpc.ScalaClient
+import org.msgpack.annotation.Message
+import org.msgpack.MessagePack
 
 /**
  * Created by kazz on 2014/04/19.
  */
 object Server {
   def main(args: Array[String]): Unit = {
+    @Message
+    class TutorialProtocol {
+      var id: Integer = 0
+      var name: String = ""
+    }
+
     class TutorialServer {
-      def hello(message: String, value: Int) {
-        println("called hello: " + message + " " + value)
-        sys.exit(1)
+      def hello(int_val: Int, list: List[Integer], map: Map[String, String]): String = {
+        val result = "called hello: " + int_val + " " + list + " " + map
+        println(result)
+        result
+      }
+      def call_tutorial(protocol: TutorialProtocol): String = {
+        val result = "id: " + protocol.id + "\n" + "name: " + protocol.name
+        println(result)
+        result
       }
     }
     val port = 12345
     ScalaServer().withServeObject(new TutorialServer()).listen(port)
 
     // Client
-    trait TutorialInterface {
-      def hello(message: String, value: Int)
-    }
-
-    val client = ScalaClient("localhost", port)
-    val proxy: TutorialInterface = client.proxy[TutorialInterface]
-    proxy.hello("test message", 100)
+//    trait TutorialInterface {
+//      def hello(message: String, value: Int)
+//    }
+//
+//    val client = ScalaClient("localhost", port)
+//    val proxy: TutorialInterface = client.proxy[TutorialInterface]
+//    proxy.hello("test message", 100)
   }
 }
